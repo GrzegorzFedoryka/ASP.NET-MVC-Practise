47efe4510336a0e34837af5b6d764d572dedec89
GrzegorzFedoryka <gfedoryka@gmail.com>
2023-04-01 13:24:35 +0200

    added migration and dummy cs in db-context

--- CarWorkshop.Infrastructure/Persistance/CarWorkshopDbContext.cs
+++ CarWorkshop.Infrastructure/Persistance/CarWorkshopDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CarWorkshop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,14 @@ public class CarWorkshopDbContext : DbContext
 {
     public DbSet<Domain.Entities.CarWorkshop> CarWorkshops { get; set; }
 
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=localhost,1433;Database=CarWorkshopDb;Integrated Security=false;User ID=sa;Password=SqlServer123!;TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Domain.Entities.CarWorkshop>()
+            .OwnsOne(e => e.ContactDetails);
+    }
 }
