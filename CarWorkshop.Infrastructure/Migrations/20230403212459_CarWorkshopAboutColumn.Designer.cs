4c9da25b90e51d9716aec3f57086b3d459af60fc
GrzegorzFedoryka <gfedoryka@gmail.com>
2023-04-03 23:26:11 +0200

    added about column to CarWorkshops

--- CarWorkshop.Infrastructure/Migrations/20230403212459_CarWorkshopAboutColumn.Designer.cs
+++ CarWorkshop.Infrastructure/Migrations/20230403212459_CarWorkshopAboutColumn.Designer.cs
@@ -4,6 +4,7 @@ using CarWorkshop.Infrastructure.Persistance;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarWorkshop.Infrastructure.Migrations
 {
     [DbContext(typeof(CarWorkshopDbContext))]
-    partial class CarWorkshopDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230403212459_CarWorkshopAboutColumn")]
+    partial class CarWorkshopAboutColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
