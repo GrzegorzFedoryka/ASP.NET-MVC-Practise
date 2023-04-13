2a8c939a596fa04bbae2c4f0fcf78d1ff4a7ae5a
GrzegorzFedoryka <gfedoryka@gmail.com>
2023-04-13 23:16:12 +0200

    added CreateWorkshop action

---
+++ CarWorkshop.Application/Services/CarWorkshopService.cs
@@ -0,0 +1,27 @@
+﻿using CarWorkshop.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Application.Services;
+
+public interface ICarWorkshopService
+{
+    Task CreateAsync(Domain.Entities.CarWorkshop carWorkshop);
+}
+
+public class CarWorkshopService : ICarWorkshopService, IServiceMarker
+{
+    private readonly ICarWorkshopRepository _carWorkshopRepository;
+
+    public CarWorkshopService(ICarWorkshopRepository carWorkshopRepository)
+    {
+        _carWorkshopRepository = carWorkshopRepository;
+    }
+    public async Task CreateAsync(Domain.Entities.CarWorkshop carWorkshop)
+    {
+        await _carWorkshopRepository.CreateAsync(carWorkshop);
+    }
+}
---
+++ CarWorkshop.MVC/Controllers/CarWorkshopController.cs
@@ -0,0 +1,21 @@
+using CarWorkshop.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarWorkshop.MVC.Controllers;
+
+public class CarWorkshopController : Controller
+{
+    private readonly ICarWorkshopService _carWorkshopService;
+
+    public CarWorkshopController(ICarWorkshopService carWorkshopService)
+    {
+        _carWorkshopService = carWorkshopService;
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(Domain.Entities.CarWorkshop carWorkshop)
+    {
+        await _carWorkshopService.CreateAsync(carWorkshop);
+
+        return RedirectToAction(nameof(Create)); //TODO: refactor
+    }
+}
